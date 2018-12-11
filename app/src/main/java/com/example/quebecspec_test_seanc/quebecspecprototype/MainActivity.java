80bd6b3d60633c4a02bfc6bcf05afe1351e96d91
Sean Celestin <sean.celestrial@gmail.com>
2018-12-10 21:17:14 -0500

    Removed layoud tags for data binding Implemented persistance for activity on rotation Implemented click on industrial inspection to new activity
    Next implement functionality to return from report.

--- app/src/main/java/com/example/quebecspec_test_seanc/quebecspecprototype/MainActivity.java
+++ app/src/main/java/com/example/quebecspec_test_seanc/quebecspecprototype/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.quebecspec_test_seanc.quebecspecprototype;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IndustrialInspectionConfig();
+    }
+
+    public void IndustrialInspectionConfig(){
+        ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+        ib.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.industrial_inspection);
+            }
+        });
     }
 }
