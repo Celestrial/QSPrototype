71341f781cf37376f2ec6eba96be6b49128221d7
Sean Celestin <sean.celestrial@gmail.com>
2019-01-13 13:08:19 -0500

    Adding xml file of questions and answers

---
+++ app/src/main/java/com/example/quebecspec_test_seanc/quebecspecprototype/IndustrialInspection.java
@@ -0,0 +1,18 @@
+package com.example.quebecspec_test_seanc.quebecspecprototype;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IndustrialInspection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.industrial_inspection);
+
+        configureBackButton();
+    }
+
+    private void configureBackButton() {
+    }
+}
